c51ff45168bd6d203db943bc357cb8f905cab54d
bimoai857 <68723052+bimoai857@users.noreply.github.com>
2021-10-05 11:54:19 +0545

    Update SqlDataExploration.sql

--- SqlDataExploration.sql
+++ SqlDataExploration.sql
@@ -3,6 +3,8 @@ Covid 19 Data Exploration
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 */
 
+--Dataset Available at https://ourworldindata.org/covid-deaths 
+--Download the entire dataset from the beginning date
 Select *
 From PortfolioProject1..CovidDeaths
 Where continent is not null 
